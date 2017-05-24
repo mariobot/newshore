c69d1e9d6838888eac913b83b38ef1ac7d3eaad5
Mario Andres Botero Florez <mariobot113@hotmail.com>
2017-05-24 12:10:26 -0500

    First protection, publish complete solution

---
+++ WebApp/WebApp/Controllers/UploadController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult index() {
+            IEnumerable<string> files = Directory.EnumerateFiles(Server.MapPath("~/App_Data/uploads"));
+            return View(files);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (file != null){
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Resultados()
+        {
+            
+            Dictionary<string, string> resultado = new Dictionary<string, string>();
+
+            if (Utils.Validation.ContenidoFile(HttpContext,"CONTENIDO")&&Utils.Validation.ContenidoFile(HttpContext, "REGISTRADOS")){
+                resultado = Utils.Validation.Resultado(HttpContext);
+            }
+                        
+            return View(resultado);
+        }
+
+        public ActionResult Archivo(string file)
+        {
+            Dictionary<int,string> Data = Utils.Validation.TextReader(HttpContext , file);            
+
+            return View(Data);
+        }
+    }
+}
---
+++ WebApp/WebApp/Utils/Validation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Utils
+{
+    public class Validation
+    {
+        public static string value;
+
+        public static bool ContenidoFile(HttpContextBase _HttpContext, string filename)
+        {
+            var relativePath = "~/App_Data/uploads/" + filename + ".txt";
+            var absolutePath = _HttpContext.Server.MapPath(relativePath);
+            if (System.IO.File.Exists(absolutePath)){
+                return true;
+            }else{
+                return false;
+            }   
+        }
+
+        public static Dictionary<int,string> TextReader(HttpContextBase _HttpContext, string filename)
+        {
+            var relativePath = "~/App_Data/uploads/" + filename + ".txt";
+            var absolutePath = _HttpContext.Server.MapPath(relativePath);
+            var count = 0;
+            Dictionary<int, string> _Datos = new Dictionary<int,string>();            
+
+            try{
+                using (StreamReader sr = new StreamReader(absolutePath)){
+                    while (sr.Peek() >= 0){
+                        _Datos.Add(count,sr.ReadLine());
+                        count += 1;
+                    }
+                }
+            }
+            catch (Exception e){
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+
+            return _Datos;
+        }
+
+        public static Dictionary<string, string> Resultado(HttpContextBase _HttpContext)
+        {
+            Dictionary<string, string> Resultados = new Dictionary<string, string>();
+            Dictionary<int, string> _registrados = Utils.Validation.TextReader(_HttpContext, "REGISTRADOS");
+            Dictionary<int, string> contenido;            
+
+            foreach (var registro in _registrados)
+            {
+                contenido = Utils.Validation.TextReader(_HttpContext,"CONTENIDO");
+
+                foreach (char c in registro.Value)
+                {
+                    contenido = RevisarContenido(contenido, c);
+                }
+
+                if (value == registro.Value)
+                {
+                   Resultados.Add(registro.Value,"Si existe");
+                }
+                else
+                {
+                   Resultados.Add(registro.Value, "No existe");
+                }
+                value = "";
+            }
+
+            return Resultados;
+        }
+
+        public static Dictionary<int,string> RevisarContenido(Dictionary<int, string> contenido, char c)
+        {
+            foreach (var cont in contenido)
+            {
+                if (cont.Value.ToUpper() == c.ToString().ToUpper())
+                {
+                    contenido.Remove(cont.Key);
+                    value += cont.Value.ToUpper();
+                    return contenido;
+                }
+            }
+
+            return contenido;            
+        }
+    }
+}
